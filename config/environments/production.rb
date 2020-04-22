22e5f23e1b8e0b92f422b2e3881362498698fbc8
Fredrik Andersson <fredrik.andersson@aol.com>
2020-04-23 00:54:44 +0200

    Set redis

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,7 +45,9 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
-
+  config.web_socket_server_url = "wss:://oh-my-messenger.herokuapp.com/cable"
+  config.action_cable.allowed_request_origins = ["https://oh-my-messenger.herokuapp.com", "http://oh-my-messenger.herokuapp.com"]
+  
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
